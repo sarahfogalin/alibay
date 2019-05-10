8c9b000c0946b91c55e82815b598c4be263cada7
Sarah Fogalin <sarah_fogalin@hotmail.com>
2019-05-09 20:28:54 -0400

    item hover change and search items display fixed

--- src/Item.jsx
+++ src/Item.jsx
@@ -4,19 +4,19 @@ import { Link } from "react-router-dom";
 class Item extends Component {
   render = () => {
     return (
-      <div className="item-box">
-        <div className="image-container">
-          <img
-            className="image"
-            src={"http://localhost:4000/images/" + this.props.path}
-          />
-        </div>
-        <Link className="item-link" to={"/item/" + this.props.itemId}>
+      <Link className="item-link" to={"/item/" + this.props.itemId}>
+        <div className="item-box">
+          <div className="image-container">
+            <img
+              className="image"
+              src={"http://localhost:4000/images/" + this.props.path}
+            />
+          </div>
           <p className="item-name">
             {this.props.name} ${this.props.price}
           </p>
-        </Link>
-      </div>
+        </div>
+      </Link>
     );
   };
 }
